ebae50ac645b9c19613ff5e455e2bc79193e1ebb
jungggggggg <jungsu0705@naver.com>
2024-06-17 21:00:10 +0900

    Connect user avatar to stream chat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -2,12 +2,14 @@
 import { router } from 'expo-router';
 
 import { ChannelList } from "stream-chat-expo";
+import { useAuth } from '../../../providers/AuthProvider';
 
 
 export default function MainTabScreen() {
-
-    return <ChannelList
+    const {user} = useAuth();
+    return (<ChannelList
+    filters={{members: {$in: [user.id]}}}
      onSelect={(channel) => router.push(`/channel/${channel.cid}`)} 
-     />;
-    
+     />
+    );
 }
