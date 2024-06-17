095753cc3312bb460225c5cf203a4d74d476c453
jungggggggg <jungsu0705@naver.com>
2024-06-17 16:44:03 +0900

    Setup StreamChat UI

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,7 +1,14 @@
-import { Text } from "react-native"
+import { Text } from "react-native";
+import { router } from 'expo-router';
+import { useState } from "react";
+import { ChannelList, Channel, MessageList, MessageInput } from "stream-chat-expo";
+
 
 export default function MainTabScreen() {
-    return (
-        <Text>Main Tab</Text>
-    )
+    const [channel, setChannel] = useState();
+    console.log(channel);
+    return <ChannelList
+     onSelect={(channel) => router.push(`/channel/${channel.cid}`)} 
+     />;
+    
 }
